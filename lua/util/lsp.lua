d08624820862cee980717e4f976eedda5bbbbe70
KSHMR1337 <marko.zare1234@hotmail.com>
2024-06-27 16:25:43 +0200

    Updated lsp configuration

--- lua/util/lsp.lua
+++ lua/util/lsp.lua
@@ -8,6 +8,7 @@ M.on_attach = function(client, bufnr)
 	mapkey("<leader>fd", "Lspsaga finder", "n", opts) -- go to definition
 	mapkey("<leader>gd", "Lspsaga peek_definition", "n", opts) -- peak definition
 	mapkey("<leader>gD", "Lspsaga goto_definition", "n", opts) -- go to definition
+	mapkey("<leader>gS", "vsplit | Lspsaga goto_definition", "n", opts) -- go to definition
 	mapkey("<leader>ca", "Lspsaga code_action", "n", opts) -- see available code actions
 	mapkey("<leader>rn", "Lspsaga rename", "n", opts) -- smart rename
 	mapkey("<leader>D", "Lspsaga show_line_diagnostics", "n", opts) -- show  diagnostics for line
@@ -21,6 +22,10 @@ M.on_attach = function(client, bufnr)
 		mapkey("<leader>db", "DapToggleBreakpoint", "n", opts) -- toggle breakpoint
 		mapkey("<leader>dr", "DapContinue", "n", opts) -- continue/invoke debugger
 		mapkey("<leader>dt", "lua require('dap-python').test_method()", "n", opts) -- run tests
+	end
+
+	if client.name == "tsserver" then
+		mapkey("<leader>oi", "TypeScriptOrganizeImports", "n", opts) -- organise imports
 	end
 end
 
