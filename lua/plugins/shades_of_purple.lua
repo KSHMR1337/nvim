abc7b7ccc9a8a073a69e5707c1559178e1eb6733
KSHMR1337 <marko.zare1234@hotmail.com>
2024-06-27 20:56:47 +0200

    Fixed default configurations to be purple

--- lua/plugins/shades_of_purple.lua
+++ lua/plugins/shades_of_purple.lua
@@ -2,7 +2,7 @@ return {
     "Rigellute/shades-of-purple.vim",
     lazy = false,
     priority = 999,
-    -- config = function ()
-    --     vim.cmd('colorscheme shades_of_purple')
-    -- end
+    config = function ()
+        vim.cmd('colorscheme shades_of_purple')
+    end
 }
