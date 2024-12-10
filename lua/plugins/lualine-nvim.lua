ca7c814fa64d63ac5739ff5495f7309e5e62d72f
KSHMR1337 <marko.zare1234@hotmail.com>
2024-12-10 23:22:49 +0100

    Updated lualine colors.

--- lua/plugins/lualine-nvim.lua
+++ lua/plugins/lualine-nvim.lua
@@ -1,36 +1,36 @@
 local colors = {
 	black = "#251414",
-	blue = "#80a0ff",
-	green = "#9ece6a",
-	yellow = "#e0af68",
-	red = "#7B0323",
-	winered = "#ff5151",
-	bloodred = "#FF3131",
-	carmine = "#D70040",
-	crimson = "#DC143C",
+	purple = "#b2003d",
+	magenta = "#ff4d6a",
+	pink = "#ff6680",
+	light_red = "#ff3d5a",
+	dark_red = "#8b001e",
+	bright_red = "#ff1a33",
+	deep_pink = "#c8002d",
+	crimson = "#d61141",
 	grey = "#303030",
-	bg_gutter = "#6b2020",
-	fg_statusline = "#d6a9b1",
-	bg_statusline = "#361010",
-	fg_sidebar = "#ff0000",
+	bg_gutter = "#9a2434",
+	fg_statusline = "#ffd6e0",
+	bg_statusline = "#5c1724",
+	fg_sidebar = "#ff5c8a",
 }
 
 local cyber_meltdown = {
 
 	normal = {
-		a = { bg = colors.winered, fg = colors.black },
-		b = { bg = colors.bg_gutter, fg = colors.winered },
+		a = { bg = colors.light_red, fg = colors.black },
+		b = { bg = colors.bg_gutter, fg = colors.light_red },
 		c = { bg = colors.bg_statusline, fg = colors.fg_sidebar },
 	},
 
 	insert = {
-		a = { bg = colors.bloodred, fg = colors.black },
-		b = { bg = colors.bg_gutter, fg = colors.bloodred },
+		a = { bg = colors.bright_red, fg = colors.black },
+		b = { bg = colors.bg_gutter, fg = colors.bright_red },
 	},
 
 	command = {
-		a = { bg = colors.carmine, fg = colors.black },
-		b = { bg = colors.bg_gutter, fg = colors.carmine },
+		a = { bg = colors.deep_pink, fg = colors.black },
+		b = { bg = colors.bg_gutter, fg = colors.deep_pink },
 	},
 
 	visual = {
@@ -39,12 +39,12 @@ local cyber_meltdown = {
 	},
 
 	replace = {
-		a = { bg = colors.red, fg = colors.black },
-		b = { bg = colors.bg_gutter, fg = colors.red },
+		a = { bg = colors.dark_red, fg = colors.black },
+		b = { bg = colors.bg_gutter, fg = colors.dark_red },
 	},
 
 	inactive = {
-		a = { bg = colors.bg_statusline, fg = colors.blue },
+		a = { bg = colors.bg_statusline, fg = colors.purple },
 		b = { bg = colors.bg_statusline, fg = colors.bg_gutter, gui = "bold" },
 		c = { bg = colors.bg_statusline, fg = colors.bg_gutter },
 	},
